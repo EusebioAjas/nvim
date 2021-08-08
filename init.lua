f574394cd69e3d373c326ca8c57255078d5e837a
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-08-08 14:16:23 -0500

    add init.lua
    - require a general settings
    - require plugings
    - require lsp

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- Plugins
+require('plugins')
+
+-- General
+require('keymappings')
+require('settings')
+require('colorscheme')
+
+-- Plugins
+require('nv-compe')
+require('nv-galaxyline')
+require('nv-colorizer')
+require('nv-bufferline')
+require('nv-gitsigns')
+require('nv-autopairs')
+require('nv-telescope')
+
+-- LSP 
+require('lsp.solargraph-ls');
+require('lsp.tsserver-ls');
+require('lsp.bash-ls');
