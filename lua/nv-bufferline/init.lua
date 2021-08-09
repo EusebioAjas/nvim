6eab5af18956b743b4c5a1be112f0016e8208902
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-08-08 20:35:37 -0500

    add keymappings for bufferline
    - open buffer line pick
    - close buffer line pick

--- lua/nv-bufferline/init.lua
+++ lua/nv-bufferline/init.lua
@@ -1,5 +1,7 @@
 -- tab switch buffer
 require("bufferline").setup{}
 vim.api.nvim_set_keymap('n', '<TAB>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferLineCyclePrev>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>p', ':BufferLinePick<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>pd', ':BufferLinePickClose<CR>', { noremap = true, silent = true })
 
