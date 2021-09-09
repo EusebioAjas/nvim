0edd2b5da78ca77362628527cf066cb1b0d92a4b
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-09-09 18:18:06 -0500

    Deprecate use_internal_diff
    - Update to diff_opts

--- lua/nv-gitsigns/init.lua
+++ lua/nv-gitsigns/init.lua
@@ -41,5 +41,8 @@
   update_debounce = 100,
   status_formatter = nil, -- Use default
   word_diff = false,
-  use_internal_diff = true,  -- If luajit is present
+  diff_opts = {
+    internal = true,  -- If luajit is present
+  }
 }
+
