617eb0ebf4611ddecc19dafff414e9653598db9f
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-03 17:49:37 -0500

    Update lsp-saga

--- lua/plugins/lspsaga/init.lua
+++ lua/plugins/lspsaga/init.lua
@@ -10,8 +10,8 @@ saga.init_lsp_saga {
 
 local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap('n', '<C-e>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.api.nvim_set_keymap('n', '<C-g>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-d>', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.api.nvim_set_keymap('i', '<C-s>', '<Cmd>Lspsaga signature_help<CR>', opts)
+vim.api.nvim_set_keymap('i', '<C-l>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.api.nvim_set_keymap('n', 'gh', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 
