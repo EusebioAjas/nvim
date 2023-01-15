006afdb84530ca045761d8fdecb32550a06072aa
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-01-15 12:27:48 -0600

    add lsp-zero
    - remove individual language server config
    - add lsp-zero to manage lsp

--- lua/lsp/init.lua
+++
@@ -1,6 +0,0 @@
-require('lsp.solargraph-ls')
-require('lsp.tsserver-ls')
-require('lsp.angular-ls')
-require('lsp.bash-ls')
-require('lsp.css-ls')
-
--- lua/packerplugins.lua
+++ lua/packerplugins.lua
@@ -11,42 +11,64 @@ end
 return require('packer').startup(function()
   -- Packer can manage itself
   use {'wbthomason/packer.nvim', opt = true}
+
   -- Nvimtree
   use 'kyazdani42/nvim-tree.lua'
-  -- LSP config
-  use 'neovim/nvim-lspconfig'
-  use 'glepnir/lspsaga.nvim'
-  use 'onsails/lspkind-nvim'
-  -- Autocomplete and languge pack
-  use 'hrsh7th/nvim-compe'
-  use 'hrsh7th/vim-vsnip'
-  -- formatter
-  use 'mhartington/formatter.nvim'
+
+  -- LSP Zero
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+      {'saadparwaiz1/cmp_luasnip'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+
+      -- Snippet Collection (Optional)
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
   -- Treesitter
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
   use 'nvim-treesitter/playground'
   use 'p00f/nvim-ts-rainbow'
+
   -- Icons
   use 'kyazdani42/nvim-web-devicons'
+
   -- Galaxy line 
   use 'glepnir/galaxyline.nvim'
+
   -- Telescope
   use 'nvim-lua/popup.nvim'
   use 'nvim-telescope/telescope.nvim'
+
   -- Colorscheme
   use 'christianchiarulli/nvcode-color-schemes.vim'
   use 'norcalli/nvim-colorizer.lua'
   use 'unblevable/quick-scope'
+
   -- BufferLine
   use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
-  -- Ruby and Rails 
-  use 'tpope/vim-endwise'
-  use 'tpope/vim-rails'
+
   -- es6 and react snippets
-  use {'dsznajder/vscode-es7-javascript-react-snippets', run = 'yarn install --frozen-lockfile && yarn compile' }
+  -- use {'dsznajder/vscode-es7-javascript-react-snippets', run = 'yarn install --frozen-lockfile && yarn compile' }
+
   -- Git
   use 'TimUntersberger/neogit'
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+
   -- General plugins
   use 'windwp/nvim-autopairs'
   use 'kevinhwang91/nvim-bqf'
