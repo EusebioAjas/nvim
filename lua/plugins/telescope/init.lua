6beb9f4cef87ab34c57f41294aae85c19c5067dd
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-01-23 18:46:51 -0600

    update plugins settings

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,8 +1,9 @@
-require('telescope').setup{}
-
+local builtin = require('telescope.builtin')
 local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>', opts)
+vim.keymap.set('n', '<leader>pf', builtin.find_files, opts)
+vim.keymap.set('n', '<C-p>', builtin.git_files, opts)
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, opts)
+vim.keymap.set('n', '<leader>pb', builtin.buffers, opts)
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, opts)
+
