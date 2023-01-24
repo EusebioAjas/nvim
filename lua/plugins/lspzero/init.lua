27467d8e008c50f038667e1a91311312bd6294e1
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-01-23 18:42:25 -0600

    update lspzero settings

--- lua/plugins/lspzero/init.lua
+++ lua/plugins/lspzero/init.lua
@@ -1,4 +1,5 @@
 local lsp = require('lsp-zero')
+
 lsp.preset('recommended')
 
 lsp.ensure_installed({
@@ -7,9 +8,45 @@ lsp.ensure_installed({
   'tsserver',
 })
 
-lsp.on_attach(
-  function(client, bufnr)
+lsp.configure('sumneko_lua', {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim', 'use'}
+      }
+    }
+  }
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
+
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
@@ -22,7 +59,8 @@ lsp.on_attach(
   end
 )
 
-lsp.nvim_workspace()
-
 lsp.setup()
 
+vim.diagnostic.config({
+  virtual_text = true,
+})
